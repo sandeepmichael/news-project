f4eb228765234ac7ad9787d308ee2638baed0fe4
unknown <mahadarisandeep@gmail.com>
2021-06-23 18:25:24 +0530

    second commit

--- pages/feed/[id].js
+++ pages/feed/[id].js
@@ -14,7 +14,7 @@ export const Feed = ({pageNumber, articles}) => {
                <div key={index} className={styles.post}>
                    <h1 onClick={() => (window.location.href = article.url)}>{article.title}</h1>
                    <p>{article.description}</p>
-                   <img  onClick={() => (window.location.href = article.url)} src={article.urlToImage} />
+                   <img   src={article.urlToImage} />
 
                </div>
            ))}
